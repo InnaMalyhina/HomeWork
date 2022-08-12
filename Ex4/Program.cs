1cee5fe5a34baedc8871e14ff4f5e28e2c49c304
stopudof <stopudof@mail.ru>
2022-08-12 17:12:11 +0300

    Ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N(включительно).*/
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse (Console.ReadLine());
+int count = 2;
+
+Console.WriteLine("Чётные числа от 1 до N: ");
+
+while (count <= N)
+{Console.WriteLine(count); count += 2;}
+
